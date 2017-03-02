b61bfdf684c798b7ea6d0bef421051962d9cdd01
Thomas <tbruun@gmail.com>
2017-03-02 17:12:37 +0100

    added things to global scope

--- src/logger-factory.js
+++ src/logger-factory.js
@@ -1,44 +1,51 @@
 import { Logger } from './logger';
-let _factory;
+const __slf = global.__slf || ( global['__slf'] = {
+    _chain: [],
+    _queued: [],
+    _factory: null,
+    hasWarned: false
+  });
+
+/*let _factory;
 const _chain = [];
 const _queued = [];
-const NOOP = function (...args) { };
-
-let hasWarned = false;
+*/
 
 export class LoggerFactory {
   static getLogger(name) {
     let sink;
-    if (_factory) {
-      sink = _factory;
-    } else if (!hasWarned) {
-      hasWarned = true;
+    if (__slf._factory) {
+      sink = __slf._factory;
+    } else if (!__slf.hasWarned) {
+      __slf.hasWarned = true;
       console.log('Warning SLF: No LoggerFactory installed');
     }
     if (!sink) {
       sink = function (...args) {
-        if (_factory) {
-          _factory(...args);
+        if (__slf._factory) {
+          __slf._factory(...args);
         } else {
-          _queued[_queued.length % 100] = args;
+          __slf._queued[__slf._queued.length % 100] = args;
         }
       };
     }
-    return new Logger(name, sink, _chain);
+    return new Logger(name, sink, __slf._chain);
   }
   static setFactory(factory) {
-    if (_factory && factory) {
-      console.log('Warning SLF: Replacing installed LoggerFactory', _factory, factory);
+    if (__slf._factory && factory) {
+      console.log('Warning SLF: Replacing installed LoggerFactory', __slf._factory, factory);
     }
     if (!factory) {
-      _queued.length = 0;
+      __slf._queued.length = 0;
     }
-    _factory = factory;
-    if (_factory && _queued.length > 0) {
-      _queued.forEach(evt => {
-        _factory(evt);
+    __slf._factory = factory;
+    console.log('We got a factory', __slf._queued.length);
+    if (__slf._factory && __slf._queued.length > 0) {
+      console.log('***** dumping Q');
+      __slf._queued.forEach(evt => {
+        __slf._factory(evt);
       });
-      _queued.length = 0;
+      __slf._queued.length = 0;
     }
   }
   /**
@@ -46,6 +53,6 @@ export class LoggerFactory {
    * next should be called next(err, event);
    */
   static use(middleware) {
-    _chain.push(middleware);
+    __slf._chain.push(middleware);
   }
 }
