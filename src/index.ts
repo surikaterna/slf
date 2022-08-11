e6feaaa70f1b04aa6768bc9b0cbc804d7035b432
cazhjo <cazper.sihvonen@surikat.se>
2022-08-11 07:12:11 +0200

    fix export path

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { Logger } from './logger';
+export { Logger } from './Logger';
 export { LoggerFactory, Level, Event, NextFunc, Middleware } from './LoggerFactory';
 export { ConsoleLogger } from './ConsoleLogger';
